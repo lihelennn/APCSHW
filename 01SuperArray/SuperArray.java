58199341ac160a6131faf15a9ceca61b1f3ae669
lihelennn <helen82198@gmail.com>
2014-11-09 18:45:11 -0500

    weekend homework should be done here

--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -71,7 +71,7 @@ public class SuperArray{
     }
 
     public Object get(int index){
-	if (index < 0 || index > size()){
+	if (index < 0 || index >= data.length){
 	    System.out.println("Index is out of range.");
 	    return null;
 	}else{
@@ -79,10 +79,81 @@ public class SuperArray{
 	}
     }
 
-    public void set(int index, Object e){
+    public Object set(int index, Object e){
+	Object old = null;
+	if (index < 0 || index >= data.length){
+	    System.out.println("Index is out of range.");
+	    return null;
+	}else{
+	    old = data[index];
 	data[index] = e;
+	return old;
+	}
+
     }
 
+    public void add(int index, Object o){
+	int place = 0;
+	if (index >= data.length){
+	    resize(index + 1);
+	    data[index] = o;
+	}else{
+	    if (size() == data.length){
+		resize(data.length + 1);
+	    }
+	    while (place < data.length){
+		if (data[place] == null){
+		    place += 1;
+		}else{
+		    place = data.length;
+		    resize(data.length + 1);
+		}
+	    }
+	    place = 0;
+	    Object[]data2 = new Object[data.length];
+	    while (place < index){
+		data2[place] = data[place];
+		place += 1;
+	    }
+	    if (place == index){
+		data2[index] = o;
+		index += 1;
+	    }
+	    while (index < data.length){
+		data2[index] = data[place];
+		place += 1;
+		index += 1;
+	    }
+	    data = data2;
+	}
+    }
+    
+    public Object remove (int index){
+	int place = 0;
+	Object removed = null;
+	if (index < 0 || index >= data.length){
+	    System.out.println("Index is out of range.");
+	    return null;
+	}else{
+	    place = 0;
+	    Object[]data2 = new Object[data.length - 1];
+	    while (place < index){
+		data2[place] = data[place];
+		place += 1;
+	    }
+	    if (place == index){
+		removed = data[index];
+		index += 1;
+	    }
+	    while (place < data2.length){
+		data2[place] = data[index];
+		place += 1;
+		index += 1;
+	    }
+	    data = data2;
+	}
+	return removed;
+    }
 
 
 
@@ -105,15 +176,27 @@ public class SuperArray{
 	//	test2.clear();
 	//	System.out.println(test2.getFullLength());
 	//	System.out.println(test2.size());
-        test2.set(5, 104);
+	System.out.println(test2.set(5, 104));
+	System.out.println(test2.set(30, 999));
+	System.out.println(test2.set(-1, 999));
 	System.out.println(test2.toString());
 	System.out.println(test2.get(10));
 	System.out.println(test2.get(2));
 	System.out.println(test2.get(-1));
 	System.out.println(test2.size());
+	test2.add(11, 99);
+	test2.add(21, 999);
+	test2.add(0, 9);
+	System.out.println(test2.toString());
+	System.out.println(test2.remove(-1));
+	System.out.println(test2.remove(0));
+	System.out.println(test2.toString());
+	System.out.println(test2.remove(18));
+	System.out.println(test2.toString());
 	test2.clear();
 	System.out.println(test2.toString());
 	System.out.println(test2.size());
+	System.out.println(test2.getFullLength());
 
     }
 }
