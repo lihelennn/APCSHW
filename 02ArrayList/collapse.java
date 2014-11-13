eb4d25fa2277980a0cc95a80da5df9d6509aa630
lihelennn <helen82198@gmail.com>
2014-11-12 22:33:39 -0500

    made collapse method void and took out toString

--- 02ArrayList/collapse.java
+++ 02ArrayList/collapse.java
@@ -2,41 +2,36 @@ import java.util.*;
 
 public class collapse{
 
-    public static void collapseDuplicates(ArrayList<Integer>() L){
+    public static void collapseDuplicates(ArrayList<Integer> L){
 	int place = 1;
-	while (place < L.length){
-	    if (L[place] == L[place - 1]){
+	while (place < L.size()){
+	    if (L.get(place) == L.get(place - 1)){
 		L.remove(place);
+	    }else{
+		place += 1;
 	    }
-	    place += 1;
 	}
     }
 
-    public static String toString(ArrayList()<Integer>() L){
-	int place = 0;
-	String ans = "[ ";
-	while (place < L.length){
-	    ans += ("" + L[place] + " ");
-	    place += 1;
-	}
-	ans += " ]";
-	return ans;
-    }
-
-
+ 
     public static void main (String[]args){
-	Object[]data = new Object[10];
-	data[0] = 1;
-	data[1] = 1;
-	data[2] = 2;
-	data[3] = 1;
-	data[4] = 1;
-	data[5] = 2;
-	data[6] = 2;
-	data[7] = 4;
-	data[8] = 4;
-	data[9] = 4;
+	ArrayList<Integer>data = new ArrayList <Integer> ();
+	data.add(new Integer(9));
+	data.add(1);
+	data.add(9);
+	data.add(3);
+	data.add(3);
+	data.add(4);
+	data.add(2);
+	data.add(2);
+	data.add(2);
+	data.add(7);
+	data.add(8);
+	data.add(8);
+	data.add(8);
+	data.add(8);
+	data.add(9);
 	collapseDuplicates(data);
-	System.out.println(toString());
+	System.out.println(data);
     }
 }
