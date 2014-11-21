04891cab6b89a2cc46b3d9b772644eed954f0960
lihelennn <helen82198@gmail.com>
2014-11-20 20:48:02 -0500

    stuff did in class with add vertical

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -2,7 +2,6 @@ public class WordGrid{
     private char[][]data;
     private int rows;
     private int cols;
-
     /**Initialize the grid to the size specified and fill all of the positions
      *with spaces.
      *@param row is the starting height of the WordGrid
@@ -22,9 +21,7 @@ public class WordGrid{
 	    placeR += 1;
 	    placeC = 0;
 	}
-
     }
-
     /**Set all values in the WordGrid to spaces ' '*/
     private void clear(){
 	int placeR = 0;
@@ -37,9 +34,7 @@ public class WordGrid{
 	    placeR += 1;
 	    placeC = 0;
 	}
-
     }
-
     /**The proper formatting for a WordGrid is created in the toString.
      *@return a String with each character separated by spaces, and each row
      *separated by newlines.
@@ -59,7 +54,6 @@ public class WordGrid{
 	}
 	return str;
     }
-
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid, and must
      *have a corresponding letter to match any letters that it overlaps.
@@ -73,40 +67,101 @@ public class WordGrid{
     public boolean addWordHorizontal(String word, int row, int col){
 	int place = 0;
 	int origCol = col;
+	int origCol2 = col;
 	if (col + word.length() - 1 > cols){
 	    return false;
 	}else{
 	    while (place < word.length() && col < cols){
 		System.out.println(word.charAt(place));
-		if (data[row][col] == word.charAt(place)){
-		    place = 0;
-		    while (place < word.length()){
+		if (data[row][col] == word.charAt(place) || data[row][col] == ' '){
+		    while (origCol < cols && place < word.length()){
+			if (data[row][origCol] == word.charAt(place) || data[row][origCol] == ' '){
+			    place += 1;
+			    origCol += 1;
+			}else{
+			    return false;
+			}
+		    }
+		    place = 0 ;
+		    origCol = origCol2;
+		    while (origCol < cols && place < word.length()){
 			data[row][origCol] = word.charAt(place);
 			place += 1;
 			origCol += 1;
 		    }
 		    return true;
 		}
+		
 		place += 1;
 		col += 1;
 	    }
 	    return false;
 	}
     }
+    
 
     //vertical + diagonal should be implemented as well.
 
 
 
 
+    public boolean addWordVertical(String word, int row, int col){
+	int place = 0;
+	int origRow = row;
+	int origRow2 = row;
+	if (row + word.length() - 1 > rows){
+	    return false;
+	}else{
+	    while (place < word.length() && row < rows){
+		System.out.println(word.charAt(place));
+		if (data[row][col] == word.charAt(place) || data[row][col] == ' '){
+		    while (origRow < rows && place < word.length()){
+			if (data[origRow][col] == word.charAt(place) || data[origRow][col] == ' '){
+			    place += 1;
+			    origRow += 1;
+			}else{
+			    return false;
+			}
+		    }
+		    place = 0 ;
+		    origRow = origRow2;
+		    while (origRow < rows && place < word.length()){
+			data[origRow][col] = word.charAt(place);
+			place += 1;
+			origRow += 1;
+		    }
+		    return true;
+		}
+		
+		place += 1;
+		row += 1;
+	    }
+	    return false;
+	}
+    }
+    
+
+
+
+
+
+
+
+
     public static void main (String[]args){
-	WordGrid test1 = new WordGrid(6, 6);
-	// this works	test1.clear();
+
+
+
+	WordGrid test1 = new WordGrid(10, 10);
+	// this works test1.clear();
 	System.out.println(test1);
 	System.out.println(test1.addWordHorizontal("happy", 5, 5)); // expect false: word too long
 	System.out.println(test1.addWordHorizontal("hay", 0, 1)); // expect true with word in grid
 	System.out.println(test1.addWordHorizontal("meet", 1, 1)); // expect false; letters not in grid
 	System.out.println(test1.addWordHorizontal("hater", 2, 0)); // expect true with word in grid
+	System.out.println(test1.addWordVertical("apcs", 1, 2)); // expect true;
+	System.out.println(test1.addWordVertical("hello", 0, 0)); // expect false; not present
+	System.out.println(test1.addWordVertical("helloooo", 3, 4)); // expect false; too long
 	System.out.println(test1);
     }
 }
