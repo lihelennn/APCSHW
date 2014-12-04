b8430ec08ced58779efab454d45274bad31b3352
lihelennn <helen82198@gmail.com>
2014-12-03 21:35:33 -0500

    nevermind, added the exceptions for negative input values and default values here - small changes!

--- 03wordsearch/Driver.java
+++ 03wordsearch/Driver.java
@@ -9,24 +9,37 @@ public class Driver{
 	int seed = Rseed.nextInt();
 	int AnswerKey = 0;
 	int place = 0;
-	if (args.length > 0){
+	if (args.length >= 1){
 	    row = Integer.parseInt(args[0]);
-	    col = Integer.parseInt(args[1]);
-	    if (args.length >= 3){
-		seed = Integer.parseInt(args[2]);
-	    }
-	    if (args.length == 4){
-		AnswerKey = Integer.parseInt(args[3]);
+	    if (args.length >= 2){
+		col = Integer.parseInt(args[1]);
+		if (args.length >= 3){
+		    seed = Integer.parseInt(args[2]);
+		    if (args.length == 4){
+			AnswerKey = Integer.parseInt(args[3]);
+		    }
+		}
 	    }
 	}
-	WordGrid test1 = new WordGrid(row, col, seed);
-	test1.setSeed(seed);
-	if (AnswerKey == 1){
-	    test1.loadWordsFromFiles("words.txt", false);
-	}else{
-	    test1.loadWordsFromFiles("words.txt", true);
+	if (row == 0){
+	    row = 10; //default;
 	}
-	System.out.println("Find these words:\n" + test1.wordsInPuzzle());
-	System.out.println(test1);
+	if (col == 0){
+	    col = 10; //default;
+	}
+	if (row < 0 || col < 0 || row >= 40 || col >= 40){
+	    System.out.println("Your row/col input is either less than 0 or wayyy too large. Or maybe you don't have an input for row/col. Bad Bad Bad!!");
+	}
+	else{
+	    WordGrid test1 = new WordGrid(row, col, seed);
+	    test1.setSeed(seed);
+	    if (AnswerKey == 1){
+		test1.loadWordsFromFiles("words.txt", false);
+	    }else{
+		test1.loadWordsFromFiles("words.txt", true);
+	    }
+	    System.out.println("Find these words:\n" + test1.wordsInPuzzle());
+	    System.out.println(test1);
+	}
     }
 }
