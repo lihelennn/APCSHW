1fbab4a7b053917d00f12176b723463356f84ff1
lihelennn <helen82198@gmail.com>
2014-12-05 04:29:56 -0500

    attempt the binary search for FIND

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,24 +1,20 @@
-public class OrderedSuperArray extends SuperArray{
+import java.util.*;
 
+public class OrderedSuperArray extends SuperArray{
     String[]data;
 
     public void add(String element){
-    	System.out.println(element);
     	if (size() == 0){
     	    super.add(element);
-    	    System.out.println(this + "first element woohoo!");
     	}else{
 
     	    int place = size() ;
 	    while (place > 0){
-    		System.out.println(this);
     		if (element.compareTo(get(place - 1)) >= 0){
-    		    System.out.println("yay");
     		    super.add(place, element);
 		    place = -1;
 		}
     		place -= 1;
-    		System.out.println(data);
     	    }
 	    if (place == 0){
 		super.add(0, element);
@@ -27,27 +23,11 @@ public class OrderedSuperArray extends SuperArray{
     }
 
 
-  
-
     public String set(int index, String e){
 	String str = remove(index);
 	add(e);
 	return str;
     }
-   
-    // public void add(String o){
-    // 	int index = size();
-    // 	System.out.println(data);
-    // 	System.out.println(o);
-    // 	while( index >0 && get(index-1).compareTo(o) > 0 ){
-    // 	    index--;
-    // 	}
-    // 	super.add(index, o);
-    // }
-
-	
-		
-
 
     public void add(int index, String element){
 	add(element);
@@ -63,6 +43,33 @@ public class OrderedSuperArray extends SuperArray{
 	}
     }
 
+    public int find(String target){
+	int place = (size() / 2);
+	int ans = -1;
+	int toEnd;
+	int toStart;
+	while (place < size() && place > 0){
+	    toEnd = size() - place;
+	    toStart = place; // - 0 (not necessary; used to show)
+	    if (target.compareTo(get(place)) == 0){
+		ans = place;
+	    }
+	    if (target.compareTo(get(place)) > 0){
+		if (toEnd > toStart){
+		    place = place + (toEnd / 2);
+		}else{
+		    place = place + (toStart / 2);
+		}
+	    }else{
+		if (toEnd > toStart){
+		    place = place + (toEnd / 2);
+		}else{
+		    place = place + (toStart / 2);
+		}
+	    }
+	}
+	return ans;
+    }
 
 
     public static void main(String[]args){
@@ -72,7 +79,31 @@ public class OrderedSuperArray extends SuperArray{
 	test1.add("mmm");
 	test1.add("hhh");
 	test1.add("ccc");
+	test1.add("helen");
+	test1.set(2, "thiswasoriginallysomething");
+	System.out.println(test1);
+	test1.badInsertionSort();
 	System.out.println(test1);
     }
-	    
 }
+   
+//   public static void main(String[]args){
+// 	long startTime = System.currentTimeMillis();
+// 	OrderedSuperArray final1 = new OrderedSuperArray();
+// 	Random rand = new Random();
+// 	String tester = "aaa";
+
+// 	int count = 0;
+// 	while (count < 1000){
+// 	    tester = Character.toString((char)('a' + rand.nextInt(25))) + "aa";
+// 	    final1.add(tester);
+// 	    count += 1;
+// 	}
+// 	final1.badInsertionSort();
+// 	System.out.println(final1);
+// 	long stopTime = System.currentTimeMillis();
+// 	long elapsedTime = stopTime - startTime;
+// 	System.out.println(elapsedTime);
+
+//     }
+// }
